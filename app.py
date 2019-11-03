9b7d8cdd1547b2db046a24b8948994c28d30d6b2
SamEdwardes <edwardes.s@gmail.com>
2019-11-03 11:39:55 -0800

    updates

--- app.py
+++ app.py
@@ -13,27 +13,109 @@ import json
 import pandas as pd
 import plotly.graph_objs as go
 import plotly_express as px
+# languge
+from collections import defaultdict
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+import pprint
+from gensim import corpora
+from gensim import models
+from gensim import similarities
 
 
 ###########################################
 # GETTING DATA
 ###########################################
-df_discussions = pd.read_csv("data/2019-11-02_reddit-data-learnmath_scrubbed.csv")
-df_discussions = df_discussions.loc[:, ["discussion_topic_title", "thread_ref_link"]]
+df_discussions = pd.read_csv(
+    "data/2019-11-02_reddit-data-learnmath_scrubbed.csv")
+
+
+##############################################
+# BUILDING MODEL
+##############################################
+
+
+def convert_csv_to_list(posts_df):
+
+    # take only the combined column
+    posts_df = posts_df["combined"]
+
+    # Convert the column to a list
+    corpus_text = list()
+    for row in range(posts_df.shape[0]):
+        temp = posts_df.iloc[row]
+        corpus_text.append(temp)
+
+    # Convert the list to list of lists
+    processed_corpus = list()
+
+    stem_sentence = []
+    porter = PorterStemmer()
+
+    for text in corpus_text:
+
+        token_words = word_tokenize(text)
+        token_words
+        stem_sentence = []
+        for word in token_words:
+            stem_sentence.append(porter.stem(word))
+            stem_sentence.append(" ")
+#         # tokenize it
+#         tokenized_list = word_tokenize(text)
+
+        # convert to lower case
+        tokenized_list = [w.lower() for w in stem_sentence]
+
+        # get the alphabetic words
+        words = [word for word in tokenized_list if word.isalpha()]
+
+        # get rid of stop words
+        words = [w for w in words if not w in stop_words]
+
+        processed_corpus.append(words)
+
+    return processed_corpus  # list of lists
+
+
+def train_model(corpus):
+    dictionary_reddit = corpora.Dictionary(corpus)
+    num_words = len(dictionary_reddit.keys())
+
+    # convert to a bag of words reprensentation
+    bow_corpus_reddit = [dictionary_reddit.doc2bow(text) for text in corpus]
+
+    # train the model
+    tfidf_reddit = models.TfidfModel(bow_corpus_reddit)
+
+    # similarities model
+    index_reddit = similarities.SparseMatrixSimilarity(tfidf_reddit[bow_corpus_reddit],
+                                                       num_features=num_words)
+    return index_reddit, dictionary_reddit, tfidf_reddit
+
+
+# build model
+stop_words = stopwords.words('english')
+discussion_lists = convert_csv_to_list(df_discussions)
+model = train_model(discussion_lists)
+
 
 ###########################################
 # STATIC FUNCTIONS
 ###########################################
+
+
 def generate_table(df, max_rows=10):
     """
     Renders a table in dash app
-    
+
     Arguments:
         df {pd.DataFrame} -- Data frame to render
-    
+
     Keyword Arguments:
         max_rows {int} -- number of rows to render (default: {10})
-    
+
     Returns:
         html.Table -- table ready to be rendered by
     """
@@ -49,6 +131,7 @@ def generate_table(df, max_rows=10):
 ###########################################
 # APP LAYOUT
 ###########################################
+
 
 # COLOUR AND STYLE
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -98,6 +181,8 @@ app.layout = html.Div(style={'backgroundColor': colors['light_grey']}, children=
 ###########################################
 # APP CALL BACKS
 ###########################################
+
+
 @app.callback(
     Output(component_id='topic_prediction', component_property='children'),
     [Input(component_id='topic_title', component_property='value')]
@@ -108,6 +193,5 @@ def update_output_div(input_value):
     return 'You\'ve entered "{}"'.format(topic_string)
 
 
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
